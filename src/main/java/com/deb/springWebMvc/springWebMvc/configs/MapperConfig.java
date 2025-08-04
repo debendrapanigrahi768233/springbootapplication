daa46c14909b2a519b479cb046a945b9204a321d
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2025-08-05 01:22:49 +0530

    test

--- src/main/java/com/deb/springWebMvc/springWebMvc/configs/MapperConfig.java
+++ src/main/java/com/deb/springWebMvc/springWebMvc/configs/MapperConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
+
+
     @Bean
     public ModelMapper getModelMapper(){
         return new ModelMapper();
